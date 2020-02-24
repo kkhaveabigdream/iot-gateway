800dae85902acaec29f36e657efc87f3eb85b997
kkhaveabigdream <shen.k@husky.neu.edu>
2020-02-24 16:29:36 -0500

    lab05 final commit

--- src/main/java/neu/kaishen/connecteddevices/common/SensorDataListener.java
+++ src/main/java/neu/kaishen/connecteddevices/common/SensorDataListener.java
@@ -1,5 +1,9 @@
 package neu.kaishen.connecteddevices.common;
 
+
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+
 import redis.clients.jedis.JedisPubSub;
 
 public class SensorDataListener extends JedisPubSub{
@@ -7,6 +11,7 @@ public class SensorDataListener extends JedisPubSub{
 	SensorData sensorData = null;
 	DataUtil dataUtil = null;
 	PersistenceUtil persistenceUtil = null;
+	//public Logger log1 = LoggerFactory.getLogger(SensorDataListener.class);
 	
 
 	public SensorDataListener() {
@@ -17,16 +22,20 @@ public class SensorDataListener extends JedisPubSub{
 
 	@Override
 	public void onMessage(String channel, String message) {
+		System.out.println("Listening on Redis, jsonSensorData retrieved from Redis...");
 		System.out.println(channel + "=" + message);
+		//	log1.info(message);
 		sensorData = dataUtil.jsonToSensorData(message);
 		if (sensorData.curValue >20) {
 			actuatorData.setName("Temperature");
 			actuatorData.setCommand("Increasing");
 			actuatorData.curValue = sensorData.curValue;
+			System.out.println("Current temperature exceeds nonminalTemp by > 20. Triggering alert...");
 		}else if(sensorData.curValue <20) {
 			actuatorData.setName("Temperature");
 			actuatorData.setCommand("Decreasing");
 			actuatorData.curValue = sensorData.curValue;
+			System.out.println("Current temperature falls below nonminalTemp by < 20. Triggering alert...");
 		}
 		//System.out.println(actuatorData.curValue + actuatorData.getCommand());
 		persistenceUtil.writeActuatorDataToRedis(actuatorData);
